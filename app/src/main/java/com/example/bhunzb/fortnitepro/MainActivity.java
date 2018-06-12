c5123c73380ce8b43ef6ed538cdc65990e468756
Benaja <benhu00@outlook.com>
2018-06-12 11:30:08 +0200

    tst

--- app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
+++ app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bhunzb.fortnitepro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.);
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    */
 }
