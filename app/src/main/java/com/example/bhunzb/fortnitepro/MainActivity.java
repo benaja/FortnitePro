ab1b96d9da1aec3fa43b04387de176691d3ab913
Hunzinger Benaja <bhunzb@bbc-be.local>
2018-06-18 08:44:11 +0200

    addet favourit list

--- app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
+++ app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.bhunzb.fortnitepro;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.favourit_list,
+                R.id.Itemname,itemname));
     }
 /*
     @Override
