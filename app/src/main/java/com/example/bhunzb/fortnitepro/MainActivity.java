2b0ba26682d0c1b554536e03e0f6d0411056f7a2
noah.porfido <noah.porfido@icloud.com>
2018-06-18 16:42:57 +0200

    search in progress

--- app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
+++ app/src/main/java/com/example/bhunzb/fortnitepro/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.bhunzb.fortnitepro;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    private Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
+            }
+        });
     }
+
+
+
+
+
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
